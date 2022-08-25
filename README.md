144b75330d41d6a3f834006a9490b2fc068516c0
Artur Zabeyvorota <artur.zabeyvorota@ruter.no>
2022-08-25 22:42:31 +0200

    readme

---
+++ README.md
@@ -0,0 +1,1 @@
+### Sample spring graphql kotlin app
--- build.gradle.kts
+++ build.gradle.kts
@@ -23,9 +23,6 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework:spring-webflux")
-	testImplementation("org.springframework.graphql:spring-graphql-test")
 }
 
 tasks.withType<KotlinCompile> {
